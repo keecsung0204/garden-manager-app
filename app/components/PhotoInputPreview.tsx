f3c78c293f89019862328bcf192ccb9da53dd1e3
Ben <keecsung@gmail.com>
2026-07-02 08:39:44 -0700

    Open camera for photo input

--- app/components/PhotoInputPreview.tsx
+++ app/components/PhotoInputPreview.tsx
@@ -34,6 +34,7 @@ export default function PhotoInputPreview() {
         name="photo"
         type="file"
         accept="image/*"
+        capture="environment"
         onChange={handlePhotoChange}
       />
 
